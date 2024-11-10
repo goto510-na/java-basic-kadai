43a4a0a89ed5287214e1e22f0eded96246b4348a
goto510-na <nahoko.59124.bkto@gmail.com>
2024-11-10 18:07:13 +0900

    課題18をアップロード

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		
+	
+	KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+	taro.setGivenName();
+	taro.execIntriduce();
+	
+	KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18();
+	Ichiro.setGivenName();
+	Ichiro.execIntriduce();
+	
+	KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18();
+	Hanako.setGivenName();
+	Hanako.execIntriduce();
+	}
+}
