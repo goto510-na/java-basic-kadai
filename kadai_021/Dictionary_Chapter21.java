f60f90c47481a545ecb15449788299d77f82751f
goto510-na <nahoko.59124.bkto@gmail.com>
2024-11-24 22:55:47 +0900

    kadai_021アップロード

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,17 @@
+package kadai_021;
+
+public class Dictionary_Chapter21 {
+
+	public static void main(String[] args) {
+		
+		
+			Dictionary_Chapter21 dictionary  = new Dictionary_Chapter21();
+			String[] dictionaryArray= {"apple","banana", "grape", "orange"};
+			dictionary.search(dictionaryArray);
+			
+           
+		}
+
+	}
+
+
